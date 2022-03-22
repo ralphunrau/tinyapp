0f8516ed9093fdf0687696e30a7c29482b4bc086
Ralph Unrau <xralphunrau@gmail.com>
2022-03-22 02:33:43 +0000

    Added routing to the longURL from the shortened

--- express_server.js
+++ express_server.js
@@ -35,6 +35,11 @@ app.get('/urls/:shortURL', (req,res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
