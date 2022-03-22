4eceb3c887f4550a2bc8edd4f056210f196fd13e
Ralph Unrau <xralphunrau@gmail.com>
2022-03-22 23:43:17 +0000

    Adding a delete option

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
+app.post('/urls/:shortURL/delete', (req,res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
